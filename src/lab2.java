7953a9d7d441a63eff7e597d6bdf41b4d029a4bd
BoLiupro <94628811+BoLiupro@users.noreply.github.com>
2022-10-24 11:44:06 +0800

    Initial commit

--- src/lab2.java
+++ src/lab2.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class lab2 {
 	public static String code="";
@@ -16,5 +18,26 @@ public class lab2 {
         	code+=line;
         	line=bufferedReader.readLine();
         }
+	}
+
+	//check total key word
+	public static void level1() {
+		String keywords="abstract、assert、boolean、break、byte、case、"
+				+ "catch、char、class、continue、default、do、double、else、"
+				+ "enum、extends、final、finally、float、for、if、implements、"
+				+ "import、int、interface、instanceof、long、native、new、"
+				+ "package、private、protected、public、return、short、static、"
+				+ "strictfp、super、switch、synchronized、this、throw、throws、"
+				+ "transient、try、void、volatile、while";//all keywords
+		String []keyArr=keywords.split("、");
+		int total_num=0;
+		for(int i=0;i<keyArr.length;i++) {
+			Pattern p=Pattern.compile("[^a-z]"+keyArr[i]+"[^a-z]");
+			Matcher matcher=p.matcher(code);
+			while(matcher.find()) {
+				total_num++;
+			}
+		}
+		System.out.println("total num is "+total_num);
 	}
 }
